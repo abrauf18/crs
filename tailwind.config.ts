5f47893c30a1c9a31994ccf54927821a74025396
abrauf18 <abrauf.dev@gmail.com>
2023-12-12 11:40:29 +0500

    update tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import colors from "tailwindcss/colors";
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -9,22 +8,18 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    colors: {
-      "light-gray": "#F5F5F5",
-      "primary-color": "#F59A3B",
-      "dark-gray": "#85878D",
-      ...colors,
-    },
-    container: {
-      center: true,
-      padding: "2rem",
+    extend: {
+      colors: {
+        "light-gray": "#F5F5F5",
+        "primary-color": "#F59A3B",
+        "dark-gray": "#85878D",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
       screens: {
         "2xl": "1400px",
-      },
-    },
-    extend: {
-      screens: {
         mobile: { max: "767px" },
       },
       keyframes: {
