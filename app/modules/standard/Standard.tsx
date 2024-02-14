f552c9201ae3827c2fa5930d4edf2f724c2ffa3b
abrauf18 <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-02-14 18:48:59 +0500

    corrects filter topic in learning plans in teacher view

--- app/modules/standard/Standard.tsx
+++ app/modules/standard/Standard.tsx
@@ -108,8 +108,8 @@ function Standard({ isShownFromTeacher }: { isShownFromTeacher?: boolean }) {
     return (
         <>
             <Filters
-                text="40 Learning Standards In Total"
-                secondButtonText="Create New"
+                text={isShownFromTeacher? "40 Learning Plans In Total" : "40 Learning Standards In Total"}
+                secondButtonText={isShownFromTeacher? "Create New Plan": "Create New"}
                 handleClick={() => handleClick()}
                 isHideFirstBtn={isShownFromTeacher}
             />
