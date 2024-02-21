a80a17c35aaea97a9b2e0893c56146a608cb449c
abrauf18 <abrauf.dev@gmail.com>
2024-02-21 18:16:14 +0500

    fix: error messages

--- lib/features/auth/authAction.ts
+++ lib/features/auth/authAction.ts
@@ -6,17 +6,23 @@ interface LoginPayload {
     password: string;
 }
 
-const login = createAsyncThunk('user/login', async ({email, password}: LoginPayload) => {
-    const response = await axios
-        .post('http://localhost:8000/auth/login', {
-            email,
-            password,
-        })
-        
-    console.log(response.data);
-
-    return response.data;
-})
+const login = createAsyncThunk(
+    'user/login',
+    async ({ email, password }: LoginPayload, { rejectWithValue }) => {
+        try {
+            const response = await axios.post(
+                'http://localhost:8000/auth/login',
+                { email, password }
+            );
+            return response.data;
+        } catch (error: Error | any) {
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message);
+            } else {
+                return rejectWithValue(error.message);
+            }
+        }
+    }
+);
 
 export default login;
-
--- lib/features/auth/authSlice.ts
+++ lib/features/auth/authSlice.ts
@@ -1,54 +1,46 @@
-import {createSlice, createAsyncThunk, PayloadAction} from '@reduxjs/toolkit';
-// import axios from 'axios';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import login from './authAction';
 
 type User = {
-    id: string,
-    name: string,
-    email: string
-}
+    id: string;
+    name: string;
+    email: string;
+};
 
 type initialState = {
-    loading: boolean,
-    data: User,
-    error: string,
-}
+    loading: boolean;
+    data: User;
+    error: string;
+};
 
 const initialState = {
     loading: false,
     data: {},
-    error: "",
-}
-
-// export const login = createAsyncThunk('user/login', (email, password) => {
-//     return axios
-//         .post('http://localhost:8000/auth/login', {
-//             email: email,
-//             password: password
-//         })
-//         .then((response) => response.data)
-// })
+    error: '',
+};
 
 const userSlice = createSlice({
-    name: "user",
+    name: 'user',
     initialState: initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder.addCase(login.pending, (state) => {
-            state.loading = true
-        })
-        builder.addCase(login.fulfilled, (state, action: PayloadAction<User>) => {
-            state.loading = false
-            state.data = action.payload
-            state.error = '';
+            state.loading = true;
         });
+        builder.addCase(
+            login.fulfilled,
+            (state, action: PayloadAction<User>) => {
+                state.loading = false;
+                state.data = action.payload;
+                state.error = '';
+            }
+        );
         builder.addCase(login.rejected, (state, action) => {
-            state.loading = false
-            state.data = {}
+            state.loading = false;
+            state.data = {};
             state.error = action.error.message || 'Something went wrong';
         });
-    }
+    },
 });
 
 export default userSlice.reducer;
-
