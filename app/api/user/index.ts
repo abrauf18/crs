349927b8710438e1eb68dd2887c5959b5144168f
abrauf18 <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-03-07 20:27:11 +0500

    fix: remove comments regarding alternate approach from get and update user profile API

--- app/api/user/index.ts
+++ app/api/user/index.ts
@@ -1,14 +1,11 @@
 import axios from 'axios';
-// import { getServerSession } from 'next-auth';
-// import { options } from '@/app/api/auth/[...nextauth]/options';
 
 export const getUserProfileAPI = async (accessToken: string) => {
-    // const session = await getServerSession(options);
     const result = await axios.get(
         `${process.env.NEXT_PUBLIC_BASE_URL}/user/getUserProfile`,
         {
             headers: {
-                accesstoken: accessToken, // session?.user.accessToken,
+                accesstoken: accessToken,
             },
         }
     );
@@ -23,12 +20,10 @@ export const updateUserProfileAPI = async (
     email: string,
     password: string
 ) => {
-    // const session = await getServerSession(options);
-    // console.log(accessToken, image, name, email, password);
     const result = await axios.post(
         `${process.env.NEXT_PUBLIC_BASE_URL}/user/updateUserProfile`,
         {
-            accessToken, // session?.user.accessToken,
+            accessToken,
             image,
             name,
             email,
