ff763f28b5e01412af80103efe25e90398c7114d
abrauf18 <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-03-07 20:25:14 +0500

    feat: integrates API and handles ipnut change in profile

--- app/modules/setting/Profile.tsx
+++ app/modules/setting/Profile.tsx
@@ -1,15 +1,156 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
 import { Trash2 } from 'lucide-react';
+import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useRef, useState } from 'react';
+import { DEFAULT_IMAGE, validationError } from '@/lib/utils';
 import { Label } from '@/app/components/ui/label';
-import Avatar from '@/app/assets/images/UserImage.svg';
+import Input from '@/app/components/common/Input';
+import Loader from '@/app/components/common/Loader';
 import PictureIcon from '@/app/assets/icons/PictureIcon';
-import AppInput from '@/app/components/common/AppInput';
+import { getUserProfileAPI, updateUserProfileAPI } from '@/app/api/user/index';
+import { UploadProfilePicture } from '@/app/api/s3Bucket';
 
 interface MyProfileProps {
     isSchoolProfile?: boolean;
 }
+
 function Profile({ isSchoolProfile }: MyProfileProps) {
+    const { data, update } = useSession();
+    const hiddenFileInput = useRef<HTMLInputElement | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
+    const [originalImage, setOriginalImage] = useState<string>(DEFAULT_IMAGE);
+    const [profileData, setProfileData] = useState({
+        image: DEFAULT_IMAGE,
+        name: '',
+        email: '',
+        password: '',
+    });
+    const {
+        reset,
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        mode: 'onChange',
+        reValidateMode: 'onChange',
+        // defaultValues: profileData,
+    });
+
+    const handleClick = (event: React.MouseEvent) => {
+        hiddenFileInput.current?.click();
+    };
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files && event.target.files[0];
+        setSelectedFile(file);
+    };
+
+    const removeImage = () => {
+        setSelectedFile(null);
+        setProfileData({
+            ...profileData,
+            image: DEFAULT_IMAGE,
+        });
+    };
+
+    const undoFileChange = () => {
+        setSelectedFile(null);
+        if (hiddenFileInput.current) {
+            hiddenFileInput.current.value = '';
+        }
+        setProfileData({
+            ...profileData,
+            image: originalImage,
+        });
+    };
+
+    const handleReset = () => {
+        undoFileChange();
+        reset({
+            name: profileData.name,
+            email: profileData.email,
+            password: '',
+        });
+    };
+
+    const onFormSubmit = async (formData: any) => {
+        const s3BucketResponse: any = await UploadProfilePicture({
+            selectedFile,
+            originalImage,
+            userId: data?.user.id,
+        });
+
+        if (
+            !selectedFile ||
+            (selectedFile && s3BucketResponse?.status === 200)
+        ) {
+            const image = selectedFile
+                ? s3BucketResponse?.data?.url
+                : profileData.image;
+            const { name, email, password } = formData;
+            const response = await updateUserProfileAPI(
+                data?.user.accessToken || '',
+                image,
+                name,
+                email,
+                password
+            );
+            if (response.data.status !== 'success') {
+                return toast.error(response.data.error);
+            }
+            const res = await update({
+                ...data,
+                name: response.data.data.name,
+                email: response.data.data.email,
+                user: {
+                    ...data?.user,
+                    name: response.data.data.name,
+                    email: response.data.data.email,
+                    accessToken: response.data.data.accessToken,
+                },
+            });
+
+            return toast.success('Successfully updated profile');
+        }
+        return toast.error(s3BucketResponse?.message);
+    };
+
+    useEffect(() => {
+        (async () => {
+            try {
+                if (data?.user.accessToken) {
+                    setLoading(true);
+                    const APIdata = await getUserProfileAPI(
+                        data?.user.accessToken
+                    );
+                    const { name, email, password, image } = APIdata.data.data;
+                    setProfileData({
+                        image,
+                        name,
+                        email,
+                        password: '',
+                    });
+                    reset({
+                        image,
+                        name,
+                        email,
+                        password: '',
+                    });
+                    setOriginalImage(APIdata.data.data.image);
+                }
+            } catch (err) {
+                console.log('error: ', err);
+            } finally {
+                setLoading(false);
+            }
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data?.user.accessToken]);
     return (
         <section
             className={`bg-white  flex h-full w-full mt-8 lg:mt-0 ${
@@ -24,72 +165,139 @@ function Profile({ isSchoolProfile }: MyProfileProps) {
                 <h1 className="text-2xl  font-semibold mb-2 mobile:mb-4">
                     My profile
                 </h1>
-                <div className="md:flex justify-between lg:space-x-4 items-center  mobile:w-full mobile:mb-2">
-                    <div className="flex justify-center">
-                        <div className="border-2 border-light-gray rounded-full h-40 w-40 flex justify-center items-center">
-                            <div className="border-2 border-light-gray rounded-full h-36 w-36 flex justify-center items-center">
-                                <div className="border-2 border-light-gray rounded-full h-32 p-2 w-32 flex justify-center items-center">
-                                    <Image
-                                        src={Avatar}
-                                        alt="profile Image"
-                                        className="w-44 h-44"
-                                    />
+                <form onSubmit={handleSubmit(onFormSubmit)}>
+                    <div className="md:flex justify-between lg:space-x-4 items-center  mobile:w-full mobile:mb-2">
+                        <div className="flex justify-center">
+                            <div className="border-2 border-light-gray rounded-full h-40 w-40 flex justify-center items-center">
+                                <div className="border-2 border-light-gray rounded-full h-36 w-36 flex justify-center items-center">
+                                    <div className="border-2 border-light-gray rounded-full h-32 p-2 w-32 flex justify-center items-center">
+                                        <Image
+                                            src={
+                                                selectedFile
+                                                    ? URL.createObjectURL(
+                                                          selectedFile
+                                                      )
+                                                    : profileData.image // state.data.image
+                                            }
+                                            alt="profile Image"
+                                            className="rounded-full"
+                                            width={176}
+                                            height={176}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div className="flex flex-col justify-between items-center mobile:w-full mb-4 lg:mb-0">
+                            <button
+                                type="button"
+                                onClick={handleClick}
+                                className="text-white  flex space-x-2 items-center bg-primary-color font-semibold mobile:w-full mobile:p-2 md:px-6 md:py-2 border rounded-lg mt-2 cursor-pointer"
+                            >
+                                <PictureIcon width={18} height={18} />
+                                <span>Change Photo</span>
+                                <input
+                                    type="file"
+                                    id="profilePicInput"
+                                    accept=".pdf, .jpg, .jpeg, .png, .gif, .mp4, .avi, .mov, .doc, .docx, .xls, .xlsx, .ppt, .pptx"
+                                    onChange={handleFileChange}
+                                    ref={hiddenFileInput}
+                                    style={{ display: 'none' }}
+                                />
+                            </button>
+                            <button
+                                type="button"
+                                onClick={removeImage}
+                                className="text-dark-gray flex space-x-2 items-center font-semibold mobile:w-full p-2 md:px-6 md:py-2 border rounded-lg mt-2"
+                            >
+                                <Trash2 size={18} color="#E6500D" />
+                                <span>Remove Photo</span>
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-between items-center mobile:w-full mb-4 lg:mb-0">
-                        <button
-                            type="button"
-                            className="text-white  flex space-x-2 items-center bg-primary-color font-semibold mobile:w-full mobile:p-2 md:px-6 md:py-2 border rounded-lg mt-2"
-                        >
-                            <PictureIcon width={18} height={18} />
-                            <span>Change Photo</span>
-                        </button>
-                        <button
-                            type="button"
-                            className="text-dark-gray flex space-x-2 items-center font-semibold mobile:w-full p-2 md:px-6 md:py-2 border rounded-lg mt-2"
-                        >
-                            <Trash2 size={18} color="#E6500D" />
-                            <span>Remove Photo</span>
-                        </button>
+                    <div className="mt-2">
+                        <Label htmlFor="name">User Name</Label>
+                        <Input
+                            name="name"
+                            placeholder="Enter Name"
+                            type="text"
+                            errors={errors}
+                            register={register('name', {
+                                required: {
+                                    value: true,
+                                    message: validationError.REQUIRED_FIELD,
+                                },
+                            })}
+                        />
                     </div>
-                </div>
-                <div className="mb-5 w-full">
-                    <Label htmlFor="email">Username</Label>
-
-                    <AppInput type="email" id="email" placeholder="User Name" />
-                </div>
-                <div className="mb-5 w-full">
-                    <Label htmlFor="email">Email Address</Label>
-
-                    <AppInput type="email" id="email" placeholder="Email" />
-                </div>
-                <div className="mb-2 w-full">
-                    <Label htmlFor="password ">Password</Label>
-
-                    <AppInput
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                    />
-                </div>
-                {!isSchoolProfile && (
-                    <div className="md:flex md:justify-between w-full mt-5 gap-2">
-                        <button
-                            type="button"
-                            className="text-dark-gray font-semibold mobile:mb-2 w-full p-2 md:px-6 md:py-2 border rounded-lg"
-                        >
-                            Discard Changes
-                        </button>
-                        <button
-                            type="button"
-                            className="text-white bg-primary-color font-semibold w-full p-2 md:px-6 md:py-2 border rounded-lg"
-                        >
-                            Save Changes
-                        </button>
+                    <div className="mt-2">
+                        <Label htmlFor="email">Email Address</Label>
+                        <Input
+                            name="email"
+                            placeholder="Enter Email"
+                            type="email"
+                            errors={errors}
+                            register={register('email', {
+                                required: {
+                                    value: true,
+                                    message: validationError.REQUIRED_FIELD,
+                                },
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                    message: validationError.VALID_EMAIL,
+                                },
+                            })}
+                        />
                     </div>
-                )}
+                    <div className="mt-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input
+                            name="password"
+                            placeholder="Enter Password"
+                            type="password"
+                            errors={errors}
+                            register={register('password', {
+                                pattern: {
+                                    value:
+                                        // eslint-disable-next-line no-useless-escape
+                                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_\-\+=:;"'?\/>.<,{}\[\]])[a-zA-Z\d~`!@#$%^&*()_\-\+=:;"'?\/>.<,{}\[\]]{8,}$/,
+                                    message:
+                                        validationError.PASSWORD_VALIDATION_INFO_TEXT,
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: validationError.MIN_LENGTH,
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message: validationError.MAX_LENGTH,
+                                },
+                            })}
+                        />
+                    </div>
+                    {!isSchoolProfile && (
+                        <div className="md:flex md:justify-between w-full mt-5 gap-2">
+                            <button
+                                type="button"
+                                className="text-dark-gray font-semibold mobile:mb-2 w-full p-2 md:px-6 md:py-2 border rounded-lg"
+                                onClick={handleReset}
+                            >
+                                Discard Changes
+                            </button>
+                            <button
+                                type="submit"
+                                className="text-white bg-primary-color font-semibold w-full p-2 md:px-6 md:py-2 border rounded-lg"
+                            >
+                                {/* {loading ? (
+                                    <Loader color="white" size="4" />
+                                ) : (
+                                    'Save Changes'
+                                )} */}
+                                Save Changes
+                            </button>
+                        </div>
+                    )}
+                </form>
             </div>
         </section>
     );
