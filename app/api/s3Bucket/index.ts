1778e4094c30d517ad80c834070963db051f4e3e
abrauf18 <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-03-07 20:20:38 +0500

    feat: add update and delete profile picture APIs

---
+++ app/api/s3Bucket/index.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+export const UploadProfilePicture = async ({
+    selectedFile,
+    originalImage,
+    userId,
+}: {
+    selectedFile: any;
+    originalImage: string;
+    userId: string;
+}) => {
+    if (selectedFile) {
+        try {
+            await DeleteProfilePicture(originalImage);
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+            formData.append('fileSaveDirectory', 'ProfilePictures');
+            formData.append('userId', userId);
+
+            const response = await axios.post(
+                'http://localhost:3000/api/s3Bucket',
+                formData
+            );
+
+            if (response.status === 200) {
+                return response;
+            }
+        } catch (error) {
+            return {
+                status: 500,
+                message: 'Error Uploading Image',
+            };
+        }
+    }
+    return {
+        status: 400,
+        message: 'Image Not Selected',
+    };
+};
+
+const DeleteProfilePicture = async (objectUrl: string) => {
+    try {
+        const response = await axios.delete(
+            'http://localhost:3000/api/s3Bucket',
+            {
+                data: { url: objectUrl },
+            }
+        );
+        if (response.status === 200) {
+            return response;
+        }
+        return {
+            status: 500,
+            message: 'Error Deleting Image',
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            message: 'Error Deleting Image',
+        };
+    }
+};
