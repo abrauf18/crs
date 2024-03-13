6655a531f0397076beafbb47c6b181780d772584
abrauf18 <158137112+Muhammad-Ibrahim-Tayyab@users.noreply.github.com>
2024-03-13 20:13:29 +0500

    refactor: remove redundent onchange events from Input

--- app/components/common/Input.tsx
+++ app/components/common/Input.tsx
@@ -7,7 +7,6 @@ interface InputPropsInterface {
     register: any;
     type: string;
     errors?: any;
-    handleChange?: (key: string) => void;
 }
 
 function Input({
@@ -16,7 +15,6 @@ function Input({
     register,
     type,
     errors,
-    handleChange,
 }: InputPropsInterface): JSX.Element {
     return (
         <div className="common-input  my-2 position-relative">
@@ -27,9 +25,6 @@ function Input({
                 name={name}
                 placeholder={placeholder}
                 {...register}
-                onKeyUp={(event) => {
-                    handleChange && handleChange(event.key);
-                }}
             />
             {errors[name]?.type && (
                 <div className="flex items-center gap-1 mt-1">
