1e941fc0079ab95260887ecaeb899b51801862a2
abrauf18 <abrauf.dev@gmail.com>
2024-04-15 13:32:59 +0500

    fix: handle next auth sessions

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,15 +1,22 @@
+import React from 'react';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 import { Montserrat } from 'next/font/google';
 import '@/app/globals.css';
-import React from 'react';
 import SideBar from '@/app/components/common/sidebar/Sidebar';
+import { options } from '../api/auth/[...nextauth]/options';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const session = await getServerSession(options);
+    if (!session) {
+        return redirect('/signin');
+    }
     return (
         <section className="flex flex-col md:flex-row">
             <div className="md:basis-[100px] lg:basis-[280px]  ">
