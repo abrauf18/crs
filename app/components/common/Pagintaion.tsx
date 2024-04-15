45d39c7c1942a63677267d8806d8130c4eb13333
abrauf18 <abrauf.dev@gmail.com>
2024-04-15 13:52:52 +0500

    fix: make props optional to prevent breakage in app

--- app/components/common/Pagintaion.tsx
+++ app/components/common/Pagintaion.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface PaginationProps {
-    currentPage: number;
-    totalPages: number;
-    onPageChange: (page: number) => void;
+    currentPage?: number;
+    totalPages?: number;
+    onPageChange?: (page: number) => void;
 }
 
 function Pagination({
-    currentPage,
-    totalPages,
-    onPageChange,
+    currentPage = 1,
+    totalPages = 1,
+    onPageChange = () => {},
 }: PaginationProps) {
     const maxPagesToShow = 4; // Adjust this value as needed
 
