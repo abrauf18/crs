6a7a08bdd2259ade5517b5adf35c6cc9e8edef35
abrauf18 <abrauf.dev@gmail.com>
2024-04-19 10:50:53 +0500

    feat: add file name and progress in file uploading to make it dynamic based on axios response

--- app/components/common/FileUploading.tsx
+++ app/components/common/FileUploading.tsx
@@ -4,8 +4,15 @@ import React from 'react';
 interface FileUploadingProp {
     isCompleted?: boolean;
     Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>> | LucideIcon;
+    fileName?: string;
+    progress: number;
 }
-function FileUploading({ isCompleted, Icon }: FileUploadingProp) {
+function FileUploading({
+    isCompleted,
+    Icon,
+    fileName,
+    progress,
+}: FileUploadingProp) {
     return (
         <div className="p-3 border-2 rounded-lg flex space-x-4 mt-3 items-center">
             <div className="h-full">
@@ -17,22 +24,26 @@ function FileUploading({ isCompleted, Icon }: FileUploadingProp) {
             </div>
             <div className="flex flex-col gap-1">
                 <h3 className="md:text-md text-sm font-semibold">
-                    Master Digital Product Design..
+                    {fileName || 'Master Digital Product Design..'}
                 </h3>
-                <div className="flex gap-1">
-                    <Upload size={18} color="#7AA43E" />
-                    <p className="text-sm">
-                        {!isCompleted
-                            ? 'Uploading 35%'
-                            : 'Uploaded Successfully'}
-                    </p>
-                </div>
-                <div className="w-full bg-white rounded-md">
-                    <div
-                        className="h-2 bg-primary-color rounded-md"
-                        style={{ width: !isCompleted ? '35%' : '100%' }}
-                    />
-                </div>
+                {progress !== 0 && (
+                    <>
+                        <div className="flex gap-1">
+                            <Upload size={18} color="#7AA43E" />
+                            <p className="text-sm">
+                                {progress !== 100
+                                    ? `Uploading ${progress}%`
+                                    : 'Uploaded Successfully'}
+                            </p>
+                        </div>
+                        <div className="w-full bg-white rounded-md">
+                            <div
+                                className="h-2 bg-primary-color rounded-md"
+                                style={{ width: `${progress}%` }}
+                            />
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
