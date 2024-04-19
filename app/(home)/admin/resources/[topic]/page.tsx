31065cf0cd8d28eee8931b0d35484c45c78fe9f5
abrauf18 <abrauf.dev@gmail.com>
2024-04-19 11:14:19 +0500

    feat: handle resource/[Topic] page

--- app/(home)/admin/resources/[topic]/page.tsx
+++ app/(home)/admin/resources/[topic]/page.tsx
@@ -1,8 +1,60 @@
 import React from 'react';
+import { Session, getServerSession } from 'next-auth';
+import { convertDashesToSpaces } from '@/lib/utils';
+import { getResourcesCountAPI } from '@/app/api/resource';
+import { options } from '@/app/api/auth/[...nextauth]/options';
+import UnhandledError from '@/app/modules/error/UnhandledError';
 import TopicType from '@/app/modules/resources/topicType/TopicType';
 
-function TopicPage() {
-    return <TopicType />;
+async function TopicPage({ params }: { params: { topic: string } }) {
+    const data: Session | null = await getServerSession(options);
+
+    let APIdata: {
+        slideshowCount: number;
+        videoCount: number;
+        worksheetCount: number;
+        exitTicketTestCount: number;
+        quizCount: number;
+        assignmentCount: number;
+        totalCount: number;
+    } = {
+        slideshowCount: 0,
+        videoCount: 0,
+        worksheetCount: 0,
+        exitTicketTestCount: 0,
+        quizCount: 0,
+        assignmentCount: 0,
+        totalCount: 0,
+    };
+
+    if (data) {
+        try {
+            const response = await getResourcesCountAPI({
+                accessToken: data?.user?.accessToken,
+                topic: convertDashesToSpaces(params.topic).toLowerCase(),
+            });
+
+            const APIResponse = await response.json();
+
+            if (APIResponse.status !== 'error') {
+                APIdata = APIResponse?.data;
+                return <TopicType APIdata={APIdata} />;
+            }
+
+            if (!response.ok) {
+                throw new Error(APIResponse?.message);
+            }
+        } catch (error: any) {
+            return (
+                <UnhandledError
+                    error={{
+                        message: error?.message,
+                        name: error?.name,
+                    }}
+                />
+            );
+        }
+    }
 }
 
 export default TopicPage;
--- app/modules/resources/topicType/TopicType.tsx
+++ app/modules/resources/topicType/TopicType.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
 import { HelpCircle, TicketIcon } from 'lucide-react';
 import Filters from '@/app/components/common/Filters';
 import Card from '@/app/components/common/Card';
@@ -10,8 +9,19 @@ import VideoIcon from '@/app/assets/icons/VideoIcon';
 import WorksheetIcon from '@/app/assets/icons/WorksheetIcon';
 import UploadResourceModal from '../UploadResourceModal';
 
-function TopicPage() {
-    const path = usePathname();
+function TopicPage({
+    APIdata,
+}: {
+    APIdata: {
+        slideshowCount: number;
+        videoCount: number;
+        worksheetCount: number;
+        exitTicketTestCount: number;
+        quizCount: number;
+        assignmentCount: number;
+        totalCount: number;
+    };
+}) {
     const [isShowUploadModal, setIsShowUploadModal] = useState(false);
 
     const handleOpenUploadModal = () => {
@@ -25,22 +35,39 @@ function TopicPage() {
     return (
         <section>
             <Filters
-                text="500 Resources In Total"
+                text={`${APIdata.totalCount} Resources In Total`}
                 secondButtonText="Upload Resources"
                 handleClick={handleOpenUploadModal}
+                isHideFirstBtn
             />
             <div className="grid mobile:grid-col-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4 mobile:place-items-center">
-                <Card Icon={VideoIcon} cardText="Total Video's" count="20K" />
-                <Card Icon={SlideShowIcon} cardText="Slideshows" count={200} />
-                <Card Icon={WorksheetIcon} cardText="Worksheets" count="20" />
+                <Card
+                    Icon={VideoIcon}
+                    cardText="Total Video's"
+                    count={APIdata.videoCount}
+                />
+                <Card
+                    Icon={SlideShowIcon}
+                    cardText="Slideshows"
+                    count={APIdata.slideshowCount}
+                />
+                <Card
+                    Icon={WorksheetIcon}
+                    cardText="Worksheets"
+                    count={APIdata.worksheetCount}
+                />
             </div>
             <div className="grid mobile:grid-col-1 sm:grid-cols-2 md:grid-cols-2 gap-4 mt-4 mobile:place-items-center">
                 <Card
                     Icon={TicketIcon}
                     cardText="Exit Ticket Test"
-                    count="150"
+                    count={APIdata.exitTicketTestCount}
                 />
-                <Card Icon={HelpCircle} cardText="Quizzes" count={150} />
+                <Card
+                    Icon={HelpCircle}
+                    cardText="Quizzes"
+                    count={APIdata.quizCount}
+                />
             </div>
             {isShowUploadModal && (
                 <div className="fixed right-0 top-0 z-50 w-[100%] md:w-[60%] lg:w-[30%]">
