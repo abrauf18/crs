d55f57aef23928fce3f2003471b6364971e5bf8f
abrauf18 <abrauf.dev@gmail.com>
2024-04-19 10:56:46 +0500

    feat: added handle click and button type functionality to modal footer button

--- app/components/common/ModalFooter.tsx
+++ app/components/common/ModalFooter.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-function ModalFooter({ text }: { text: string }) {
+function ModalFooter({
+    text,
+    buttonType = 'button',
+    handleClick,
+}: {
+    text: string;
+    buttonType?: 'button' | 'submit';
+    handleClick?: () => void;
+}) {
     return (
         <div className="absolute bottom-0 left-0 w-full p-3 md:py-5 lg:py-3 border bg-white">
             <div className="cursor-pointer p-2  rounded-lg bg-primary-color text-white text-center">
-                <button type="button">{text}</button>
+                <button
+                    type={buttonType === 'submit' ? 'submit' : 'button'}
+                    onClick={() => {
+                        if (handleClick) {
+                            handleClick();
+                        }
+                    }}
+                >
+                    {text}
+                </button>
             </div>
         </div>
     );
