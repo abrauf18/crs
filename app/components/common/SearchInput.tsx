e4a14dddfba4d4ecfc8e6345bf26f548ea4586db
abrauf18 <abrauf.dev@gmail.com>
2024-05-07 11:00:31 +0500

    feat: integrate get resource by name for searching in modal

--- app/components/common/SearchInput.tsx
+++ app/components/common/SearchInput.tsx
@@ -1,15 +1,33 @@
-import { SearchIcon } from 'lucide-react';
-import React from 'react';
+'use client';
 
-function SearchInput() {
+import { SearchIcon } from 'lucide-react';
+import React, { useState } from 'react';
+
+function SearchInput({
+    handleClick,
+}: {
+    handleClick?: (search: string) => void;
+}) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
     return (
         <div className="flex border   rounded-lg px-2 mobile:pr-6 w-full justify-between items-center bg-transparent ">
             <input
                 type="text"
                 className="p-2 border-none outline-none bg-transparent mobile:w-[80%]"
                 placeholder="Search..."
+                value={inputValue}
+                onChange={handleInputChange}
             />
-            <SearchIcon width={18} height={18} />
+            <SearchIcon
+                width={18}
+                height={18}
+                onClick={() => handleClick && handleClick(inputValue)}
+            />
         </div>
     );
 }
--- app/modules/standard/VideoModal.tsx
+++ app/modules/standard/VideoModal.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import React from 'react';
+import { toast } from 'react-toastify';
+import { useSession } from 'next-auth/react';
+import { FileVideoIcon } from 'lucide-react';
 import ModalFooter from '@/app/components/common/ModalFooter';
 import { ModalHeader } from '@/app/components/common/ModalHeader';
 import SearchInput from '@/app/components/common/SearchInput';
-import { FileVideoIcon } from 'lucide-react';
-import React from 'react';
+import { getResourcesByNameAPI } from '@/app/api/resource';
 import videoImage1 from '@/app/assets/images/videoImages/videoImage1.svg';
 import VideoCard, { Card } from '../video/VideoCard';
 
@@ -24,6 +29,37 @@ const cards: Card[] = [
 ];
 
 function VideoModal({ onClose }: any) {
+    const { data } = useSession();
+
+    const searchResources = async (searchInput: string) => {
+        if (!data) {
+            return;
+        }
+
+        try {
+            const APIData = await getResourcesByNameAPI({
+                accessToken: data?.user?.accessToken,
+                resourceName: searchInput,
+            });
+
+            if (!APIData.ok) {
+                const errorData = await APIData.json();
+                throw new Error(
+                    errorData?.message ??
+                        'An error occurred while fetching video data'
+                );
+            }
+
+            const responseData = await APIData.json();
+            const resources = responseData?.data ?? [];
+            console.log(resources);
+        } catch (error: any) {
+            toast.error(
+                error.message ?? 'An error occurred while searching resources'
+            );
+        }
+    };
+
     return (
         <section className="w-full bg-white h-screen p-4 shadow-md">
             <ModalHeader
@@ -35,7 +71,7 @@ function VideoModal({ onClose }: any) {
                 onClose={onClose}
             />
             <div className="mb-5">
-                <SearchInput />
+                <SearchInput handleClick={searchResources} />
             </div>
             <div className="md:h-96 h-72  overflow-y-auto px-6">
                 {' '}
