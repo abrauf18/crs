0d3c0b25f4eaf4d04a7f6613f4722d2fcf46986d
abrauf18 <abrauf.dev@gmail.com>
2024-05-09 19:39:02 +0500

    feat: handles icons and video redirection to view video

--- app/modules/standard/StandardTable.tsx
+++ app/modules/standard/StandardTable.tsx
@@ -1,14 +1,24 @@
+import React from 'react';
+import { Poppins } from 'next/font/google';
+import { Eye, LucideFileQuestion, Trash } from 'lucide-react';
+import {
+    ResourceType,
+    // resourceTypeToIcon,
+    // resourceTypeToJsxIcon,
+} from '@/lib/utils';
 import {
     TableRow,
     TableBody,
     TableCell,
     Table,
 } from '@/app/components/ui/table';
-import React from 'react';
-import { ResourceType, resourceTypeToIcon } from '@/lib/utils';
+import XlsIcon from '@/app/assets/icons/XlsIcon';
+import PptIcon from '@/app/assets/icons/PptIcon';
 import EditIcon from '@/app/assets/icons/EditIcon';
-import { Eye, LucideFileQuestion, Trash } from 'lucide-react';
-import { Poppins } from 'next/font/google';
+import TicketIcon from '@/app/assets/icons/TicketIcon';
+import RecorderIcon from '@/app/assets/icons/RecorderIcon';
+import QuestionMarkIcon from '@/app/assets/icons/QuestionMarkIcon';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 const poppins = Poppins({
     subsets: ['latin'],
@@ -32,6 +42,15 @@ interface Topic {
 }
 
 function StandardTable({ topicList }: { topicList: Topic[] }) {
+    const router = useRouter();
+
+    const viewResource = (type: ResourceType, resourceId: string) => {
+        if (type === ResourceType.VIDEO) {
+            return router.push(`/admin/video/${resourceId}`);
+        }
+        return router.push(`/admin/resource/${resourceId}`);
+    };
+
     return (
         <Table className={`text-sm mobile:text-xs ${poppins.className}`}>
             <TableBody>
@@ -43,11 +62,22 @@ function StandardTable({ topicList }: { topicList: Topic[] }) {
                             </span>
                         </TableCell>
                         <TableCell className="flex items-start gap-2 ">
-                            {
-                                resourceTypeToIcon(
-                                    topic.type as ResourceType
-                                ) as unknown as React.ReactNode
-                            }
+                            {topic.type?.toLowerCase() === 'slideshow' && (
+                                <PptIcon fill="#1ebeff" />
+                            )}
+                            {topic.type?.toLowerCase() === 'video' && (
+                                <RecorderIcon />
+                            )}
+                            {topic.type?.toLowerCase() === 'worksheet' && (
+                                <XlsIcon color="#54C3F4" />
+                            )}
+                            {topic.type?.toLowerCase() ===
+                                'exit-ticket-test' && (
+                                <TicketIcon color="#54C3F4" />
+                            )}
+                            {topic.type?.toLowerCase() === 'quiz' && (
+                                <QuestionMarkIcon />
+                            )}
                             {topic.name}
                         </TableCell>
                         <TableCell className="text-dark-gray">
@@ -55,13 +85,20 @@ function StandardTable({ topicList }: { topicList: Topic[] }) {
                         </TableCell>
                         <TableCell className="flex justify-start items-center p-0 mt-3 ml-3">
                             <div className="mr-2 bg-light-orange rounded-md p-1">
-                                <Eye color="#F59A3B" width={18} height={18} />
+                                <Eye
+                                    color="#F59A3B"
+                                    width={18}
+                                    height={18}
+                                    onClick={() =>
+                                        viewResource(
+                                            topic.type,
+                                            topic.resourceId
+                                        )
+                                    }
+                                />
                             </div>
                             <div className="mr-2 rounded-md">
                                 <EditIcon width={28} height={28} />
-                            </div>
-                            <div className="bg-red-100 rounded-md p-1">
-                                <Trash color="#E6500D" width={18} height={18} />
                             </div>
                         </TableCell>
                     </TableRow>
