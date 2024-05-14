ce60e9fc470e85f1321a5255be38e45db8be3fd0
abrauf18 <abrauf.dev@gmail.com>
2024-05-14 16:57:34 +0500

    feat: made course card dynamic

--- app/modules/standard/details/CourseCard.tsx
+++ app/modules/standard/details/CourseCard.tsx
@@ -1,32 +1,36 @@
 import { CalendarDays, File, PlayIcon } from 'lucide-react';
 import React from 'react';
 
-function CourseCard() {
+function CourseCard({
+    name,
+    videoCount,
+    otherResourcesCount,
+    courseLength,
+}: {
+    name: string;
+    videoCount: string;
+    otherResourcesCount: string;
+    courseLength: string;
+}) {
     return (
         <div className="rounded-lg border p-4 mb-4">
             <h5 className="mb-2 text-lg font-semibold tracking-tight text-gray-900">
-                JavaScript Basics
+                {name}
             </h5>
             <div>
                 <div className="flex flex-col lg:flex-row gap-2 mb-4">
                     <div className="flex gap-1  text-dark-gray text-xs">
                         <PlayIcon height={17} width={17} color="#F59A3B" />
-                        <p>Videos (15)</p>
+                        <p>Videos ({videoCount})</p>
                     </div>
                     <div className="flex gap-1  text-dark-gray text-xs">
                         <File width={17} height={17} color="#7AA43E" />
-                        <p>Exercises (10)</p>
+                        <p>Other Resources ({otherResourcesCount})</p>
                     </div>
                     <div className="flex gap-1   text-dark-gray text-xs">
                         <CalendarDays height={17} width={17} color="#54C3F4" />
-                        <p>Course Length (2 Weeks)</p>
+                        <p>Course Length ({courseLength})</p>
                     </div>
-                </div>
-            </div>
-
-            <div className="flex items-end justify-end">
-                <div className="border rounded-lg text-dark-gray px-3 py-2 text-sm font-medium text-center mr-2 lg:hover:bg-primary-color lg:hover:text-white">
-                    Details
                 </div>
             </div>
         </div>
