89c41891275b417aac184f2c08dbe6f7ee06c311
abrauf18 <abrauf.dev@gmail.com>
2024-06-03 18:54:54 +0500

    fix: upload file to s3 issue

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-              protocol: 'https',
-              hostname: 'crs-data-storage-bucket.s3.ap-southeast-2.amazonaws.com',
-              pathname: '/**',
+                protocol: 'https',
+                hostname: 'crs-data-storage-bucket.s3.ap-southeast-2.amazonaws.com',
+                pathname: '/**',
             },
         ],
     },
