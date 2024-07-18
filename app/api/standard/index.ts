91041d74cc740976eeeaae4955ea5d855a89bf20
abrauf18 <abrauf.dev@gmail.com>
2024-07-18 16:37:56 +0500

    feat: created API to get all topics of a standard

--- app/api/standard/index.ts
+++ app/api/standard/index.ts
@@ -124,3 +124,26 @@ export const getSummarizedStandardAPI = async ({
 
     return result;
 };
+
+export const getStandardTopics = async ({
+    accessToken,
+    standardId,
+}: {
+    accessToken: string;
+    standardId: string;
+}) => {
+    const result = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/standard/getStandardTopics`,
+        {
+            headers: {
+                accesstoken: accessToken,
+                standardid: standardId,
+            },
+            next: {
+                tags: ['getStandardTopics'],
+            },
+        }
+    );
+
+    return result;
+};
