aaecbab17c173658d559e0004bb1289b253aa39a
abrauf18 <abrauf.dev@gmail.com>
2024-08-28 17:14:19 +0500

    feat: removed redundent standard page

--- app/modules/students/StudentsRecordTable.tsx
+++ app/modules/students/StudentsRecordTable.tsx
@@ -34,8 +34,7 @@ function StudentsRecordTable({ students, fontSize }: StudentsRecordProp) {
     const path = usePathname();
 
     const handleClick = (id: string) => {
-        // push(`${path}/courses/${id}`);
-        push(`${path}/courses`);
+        push(`${path}/${id}`);
     };
 
     return (
