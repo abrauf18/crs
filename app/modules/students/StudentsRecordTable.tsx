f2201631155f4f0a4f99b61700793f320d1da750
abrauf18 <abrauf.dev@gmail.com>
2024-08-29 15:14:23 +0500

    feat: update induvidual student view for a teacher by replacing replacing performance in marks with resources finished and total resources

--- app/modules/students/StudentsRecordTable.tsx
+++ app/modules/students/StudentsRecordTable.tsx
@@ -16,8 +16,8 @@ import {
 interface SummarizedStandardResult {
     standardId: string;
     standardName: string;
-    totalWeightage: number;
-    obtainedWeightage: number;
+    totalResources: number;
+    finishedResources: number;
 }
 
 interface StudentsRecordProp {
@@ -52,10 +52,10 @@ function StudentsRecordTable({ students, fontSize }: StudentsRecordProp) {
                         Standard Name
                     </TableHead>
                     <TableHead className="text-dark-gray font-semibold text-sm">
-                        Total Weightage
+                        Total Resources
                     </TableHead>
                     <TableHead className="text-dark-gray font-semibold text-sm">
-                        Obtained Weightage
+                        Finished Resources
                     </TableHead>
                     <TableHead className="text-dark-gray font-semibold text-sm">
                         Action
@@ -79,10 +79,10 @@ function StudentsRecordTable({ students, fontSize }: StudentsRecordProp) {
                             </TableCell>
 
                             <TableCell className="text-dark-gray font-normal text-sm">
-                                {resource.totalWeightage}%
+                                {resource.totalResources}
                             </TableCell>
                             <TableCell className="text-dark-gray font-normal text-sm">
-                                {resource.obtainedWeightage}%
+                                {resource.finishedResources}
                             </TableCell>
                             <TableCell className="flex justify-start items-center p-0 mt-3 ml-3 gap-2">
                                 <div
